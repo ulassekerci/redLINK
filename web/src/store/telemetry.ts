101c2f3c058f6ed74c3158e2525048945302269f
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-07-10 00:49:18 +0300

    added telemetry store types

--- web/src/store/telemetry.ts
+++ web/src/store/telemetry.ts
@@ -1,18 +1,29 @@
 import { create } from 'zustand'
 
-export const useTelemetryStore = create()((set) => ({
-  data: {
-    temp: { mosfet: 0.0, motor: 0.0 },
-    current: { motor: 0.0, battery: 0.0 },
-    id: 0.0,
-    iq: 0.0,
-    dutyCycle: 0.0,
-    erpm: 0.0,
-    voltage: 0.0,
-    ampHours: { consumed: 0.0, charged: 0.0 },
-    wattHours: { consumed: 0.0, charged: 0.0 },
-    tachometer: { value: 0.0, abs: 0.0 },
-    faultCode: 0,
-  },
-  timestamp: 0,
+export interface TelemetryState {
+  temp: { mosfet: number; motor: number }
+  current: { motor: number; battery: number }
+  id: number
+  iq: number
+  dutyCycle: number
+  erpm: number
+  voltage: number
+  ampHours: { consumed: number; charged: number }
+  wattHours: { consumed: number; charged: number }
+  tachometer: { value: number; abs: number }
+  faultCode: number
+}
+
+export const useTelemetryStore = create<TelemetryState>()(() => ({
+  temp: { mosfet: 0.0, motor: 0.0 },
+  current: { motor: 0.0, battery: 0.0 },
+  id: 0.0,
+  iq: 0.0,
+  dutyCycle: 0.0,
+  erpm: 0.0,
+  voltage: 0.0,
+  ampHours: { consumed: 0.0, charged: 0.0 },
+  wattHours: { consumed: 0.0, charged: 0.0 },
+  tachometer: { value: 0.0, abs: 0.0 },
+  faultCode: 0,
 }))
