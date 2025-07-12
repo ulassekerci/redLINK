e71195c8e75c4cd64233ce48197d513a539133c9
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-07-12 18:04:53 +0300

    erpm utility functions

---
+++ web/src/utils/erpm.ts
@@ -0,0 +1,17 @@
+const polePairs = 15 // 30 poles
+const wheelDiameter = 0.5 // meters
+
+export const calculateSpeed = (erpm: number) => {
+  const wheelCircumference = 2 * Math.PI * wheelDiameter
+  const rpm = erpm / polePairs
+
+  const metersPerMinute = rpm * wheelCircumference
+  const kilometersPerHour = (metersPerMinute * 60) / 1000
+  return kilometersPerHour
+}
+
+export const calculateDistance = (eRevs: number) => {
+  const totalMechanicalRevolutions = eRevs / polePairs
+  const distance = totalMechanicalRevolutions * wheelDiameter // meters
+  return distance
+}
