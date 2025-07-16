02a9eb1568657c051c31fe32d139000453a3def0
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-07-16 13:55:38 +0300

    expo setup

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ color: 'white' }}>Hello!</Text>
+      <StatusBar hidden={false} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
