6a0858461117406555853e555ae06afcc05304bc
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-07-18 02:43:27 +0300

    socket.io client setup

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { socket } from './services/socket'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{ color: 'white' }}>Hello!</Text>
+      <Button title='send' onPress={() => socket.emit('data', 'test')} />
       <StatusBar hidden={false} />
     </View>
   )
@@ -16,5 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 48,
   },
 })
