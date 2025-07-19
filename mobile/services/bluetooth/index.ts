678c410a01ee31c441dfa43c6fbb1704b2ff0ea0
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-07-19 23:44:29 +0300

    ble-plx setup

---
+++ mobile/services/bluetooth/index.ts
@@ -0,0 +1,4 @@
+const deviceName = 'redBLE'
+const uartServiceUUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'
+const rxCharacteristicUUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
+const txCharacteristicUUID = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
