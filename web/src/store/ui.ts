9231518bff56f541001c8badaef9f2f6a835a1b3
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-08-30 23:56:11 +0300

    persistent ui state

--- web/src/store/ui.ts
+++ web/src/store/ui.ts
@@ -1,11 +1,17 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface uiState {
   aston: boolean
   toggleAston: () => void
 }
 
-export const useUIStore = create<uiState>()((set) => ({
-  aston: false,
-  toggleAston: () => set((state) => ({ aston: !state.aston })),
-}))
+export const useUIStore = create<uiState>()(
+  persist(
+    (set) => ({
+      aston: false,
+      toggleAston: () => set((state) => ({ aston: !state.aston })),
+    }),
+    { name: 'uiState' }
+  )
+)
