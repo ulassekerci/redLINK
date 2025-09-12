b10ad401aa1eb79362e33deaf4e5ced41a1eab2c
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-09-13 01:24:37 +0300

    implement relay

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,33 @@
-import { serve } from '@hono/node-server'
-import { Server as ioServer } from 'socket.io'
-import { Hono } from 'hono'
+import { RemoteSocket, Server, Socket } from 'socket.io'
 
-const app = new Hono()
-const server = serve({ fetch: app.fetch, port: 3000 })
-const io = new ioServer(server)
-
-app.get('/', (c) => {
-  return c.text('Hello!')
-})
+const io = new Server(3000)
 
 io.on('connection', (socket) => {
-  console.log(`${socket.id} connected`)
-  socket.on('data', (arg) => {
-    console.log(arg)
-  })
+  const socketName = getName(socket)
+  if (!socketName) handleClient(socket)
+  else handleSource(socket, socketName)
 })
 
-io.on('connection', (socket) => {
-  socket.on('disconnect', (reason) => {
-    console.log(`${socket.id} disconnected - reason: ${reason}`)
+const handleClient = (socket: Socket) => {
+  socket.on('hello', updateSources)
+}
+
+const handleSource = async (socket: Socket, name: string) => {
+  updateSources()
+  socket.on('source', (data) => {
+    socket.broadcast.emit(name, data)
   })
-})
+  socket.on('disconnect', updateSources)
+}
+
+const getName = (socket: Socket | RemoteSocket<any, any>) => {
+  const nameHeader = socket.handshake.headers['x-source-name']
+  if (nameHeader) return String(nameHeader)
+  else return null
+}
+
+const updateSources = async () => {
+  const sockets = await io.fetchSockets()
+  const sources = sockets.map(getName).filter(Boolean)
+  io.emit('sources', sources)
+}
