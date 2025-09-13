c659898355d1ef75aede7a988cd69257032012b2
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-09-14 02:05:01 +0300

    updated relay server to use a map to keep sources list

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,35 +1,35 @@
-import { RemoteSocket, Server, Socket } from 'socket.io'
+import { Server } from 'socket.io'
 
 const io = new Server(3000, {
   cors: { origin: '*' },
 })
 
+const sources = new Map<string, string>() // id, name
+
 io.on('connection', (socket) => {
-  const socketName = getName(socket)
-  if (!socketName) handleClient(socket)
-  else handleSource(socket, socketName)
+  broadcastSources()
+
+  socket.on('registerSource', (name: string) => {
+    sources.set(socket.id, name)
+    broadcastSources()
+  })
+
+  socket.on('sourceData', (payload) => {
+    io.to(`clients:${socket.id}`).emit('data', payload)
+  })
+
+  socket.on('disconnect', () => {
+    const isDeleted = sources.delete(socket.id)
+    if (isDeleted) broadcastSources()
+    if (isDeleted) console.log('source disconnected')
+  })
+
+  socket.on('registerClient', (sourceID) => {
+    socket.join(`clients:${sourceID}`)
+  })
 })
 
-const handleClient = (socket: Socket) => {
-  socket.on('hello', updateSources)
-}
-
-const handleSource = async (socket: Socket, name: string) => {
-  updateSources()
-  socket.on('source', (data) => {
-    socket.broadcast.emit(name, data)
-  })
-  socket.on('disconnect', updateSources)
-}
-
-const getName = (socket: Socket | RemoteSocket<any, any>) => {
-  const nameHeader = socket.handshake.headers['x-source-name']
-  if (nameHeader) return String(nameHeader)
-  else return null
-}
-
-const updateSources = async () => {
-  const sockets = await io.fetchSockets()
-  const sources = sockets.map(getName).filter(Boolean)
-  io.emit('sources', sources)
+const broadcastSources = () => {
+  const sourcesArray = Array.from(sources, ([id, name]) => ({ id, name }))
+  io.emit('sources', sourcesArray)
 }
