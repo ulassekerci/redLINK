276f6c4e36c9679a63ea1efa8a1cf4586fec48e0
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-09-14 02:06:57 +0300

    updated mobile client to register as a source

--- mobile/services/socket/index.ts
+++ mobile/services/socket/index.ts
@@ -1,14 +1,14 @@
 import { io } from 'socket.io-client'
 import { useVehicleStore } from '../../store/vehicle'
 
-const socket = io('http://172.20.10.2:3000', {
-  extraHeaders: {
-    // TODO: get name input
-    'x-source-name': 'redLink Development',
-  },
+const socket = io('http://172.20.10.2:3000')
+
+socket.on('connect', () => {
+  // TODO: get user input
+  socket.emit('registerSource', 'Ulaş')
 })
 
 export const uploadData = () => {
   const vehicleData = useVehicleStore.getState()
-  socket.emit('source', vehicleData)
+  socket.emit('sourceData', vehicleData)
 }
