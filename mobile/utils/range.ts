2d4ee4ece0c3e001e582d1b9749eb90f96799a00
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2025-09-18 02:12:29 +0300

    remove unused utils

--- mobile/utils/range.ts
+++
@@ -1,5 +0,0 @@
-export const range = (startInput: number, endInput: number) => {
-  const start = Math.round(startInput)
-  const end = Math.round(endInput)
-  return Array.from({ length: end - start + 1 }, (_, i) => start + i)
-}
